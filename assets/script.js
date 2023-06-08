981c24fc3e909d318bae26fa9846233d678a5d57
Riley19James <rikennedy@live.com>
2023-06-08 16:58:24 -0300

    Using different navbar for design.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
 var weatherAPIKey="afc152ee123b7909c41df0191e9e521a";   
 var city;
 
+http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit={limit}&appid={afc152ee123b7909c41df0191e9e521a}
 
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + afc152ee123b7909c41df0191e9e521a;
+fetch(queryURL)
 
 
 
